8560779031149f05ca5f9089995260f51e31e0f0
Uraimoff <uraimovibrohim5@gmail.com>
2022-10-10 20:49:51 +0500

    fix

--- src/Pages/Form/Form.jsx
+++ src/Pages/Form/Form.jsx
@@ -119,6 +119,111 @@ render(){
                 </tr>
             </tbody>
         </table>
+        <table className='value'>
+            <thead>
+                <tr>
+                    <th className='Id_name'>Id</th>
+                    <th className='horizont_sticky'>Name</th>
+                    <th className='horizont_sticky'>Surname</th>
+                    <th className='horizont_sticky'>Region</th>
+                    <th className='horizont_sticky'>Age</th>
+                    <th className='horizont_sticky'>Job</th>
+                    <th className='Edit_name'>Edit</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td className='id_sticky'>1</td>
+                    <td className='inner_txt qwer'>Ibrohim</td>
+                    <td className='inner_txt qwer'>Uraimov</td>
+                    <td className='inner_txt qwer'>Tashkent</td>
+                    <td className='inner_txt qwer'>19</td>
+                    <td className='inner_txt qwer'>Freelancer</td>
+                    <td className='edit_sticky'>Edit</td>
+                </tr>
+                <tr>
+                    <td className='id_sticky'>2</td>
+                    <td className='inner_txt qwer'>Ibrohim</td>
+                    <td className='inner_txt qwer'>Uraimov</td>
+                    <td className='inner_txt qwer'>Tashkent</td>
+                    <td className='inner_txt qwer'>19</td>
+                    <td className='inner_txt qwer'>Freelancer</td>
+                    <td className='edit_sticky'>Edit</td>
+                </tr>
+                <tr>
+                    <td className='id_sticky'>3</td>
+                    <td className='inner_txt qwer'>Ibrohim</td>
+                    <td className='inner_txt qwer'>Uraimov</td>
+                    <td className='inner_txt qwer'>Tashkent</td>
+                    <td className='inner_txt qwer'>19</td>
+                    <td className='inner_txt qwer'>Freelancer</td>
+                    <td className='edit_sticky'>Edit</td>
+                </tr>
+                <tr>
+                    <td className='id_sticky'>3</td>
+                    <td className='inner_txt qwer'>Ibrohim</td>
+                    <td className='inner_txt qwer'>Uraimov</td>
+                    <td className='inner_txt qwer'>Tashkent</td>
+                    <td className='inner_txt qwer'>19</td>
+                    <td className='inner_txt qwer'>Freelancer</td>
+                    <td className='edit_sticky'>Edit</td>
+                </tr>
+                <tr>
+                    <td className='id_sticky'>3</td>
+                    <td className='inner_txt qwer'>Ibrohim</td>
+                    <td className='inner_txt qwer'>Uraimov</td>
+                    <td className='inner_txt qwer'>Tashkent</td>
+                    <td className='inner_txt qwer'>19</td>
+                    <td className='inner_txt qwer'>Freelancer</td>
+                    <td className='edit_sticky'>Edit</td>
+                </tr>
+                <tr>
+                    <td className='id_sticky'>3</td>
+                    <td className='inner_txt qwer'>Ibrohim</td>
+                    <td className='inner_txt qwer'>Uraimov</td>
+                    <td className='inner_txt qwer'>Tashkent</td>
+                    <td className='inner_txt qwer'>19</td>
+                    <td className='inner_txt qwer'>Freelancer</td>
+                    <td className='edit_sticky'>Edit</td>
+                </tr>
+                <tr>
+                    <td className='id_sticky'>3</td>
+                    <td className='inner_txt qwer'>Ibrohim</td>
+                    <td className='inner_txt qwer'>Uraimov</td>
+                    <td className='inner_txt qwer'>Tashkent</td>
+                    <td className='inner_txt qwer'>19</td>
+                    <td className='inner_txt qwer'>Freelancer</td>
+                    <td className='edit_sticky'>Edit</td>
+                </tr>
+                <tr>
+                    <td className='id_sticky'>3</td>
+                    <td className='inner_txt qwer'>Ibrohim</td>
+                    <td className='inner_txt qwer'>Uraimov</td>
+                    <td className='inner_txt qwer'>Tashkent</td>
+                    <td className='inner_txt qwer'>19</td>
+                    <td className='inner_txt qwer'>Freelancer</td>
+                    <td className='edit_sticky'>Edit</td>
+                </tr>
+                <tr>
+                    <td className='id_sticky'>3</td>
+                    <td className='inner_txt qwer'>Ibrohim</td>
+                    <td className='inner_txt qwer'>Uraimov</td>
+                    <td className='inner_txt qwer'>Tashkent</td>
+                    <td className='inner_txt qwer'>19</td>
+                    <td className='inner_txt qwer'>Freelancer</td>
+                    <td className='edit_sticky'>Edit</td>
+                </tr>
+                <tr>
+                    <td className='id_sticky'>3</td>
+                    <td className='inner_txt qwer'>Ibrohim</td>
+                    <td className='inner_txt qwer'>Uraimov</td>
+                    <td className='inner_txt qwer'>Tashkent</td>
+                    <td className='inner_txt qwer'>19</td>
+                    <td className='inner_txt qwer'>Freelancer</td>
+                    <td className='edit_sticky'>Edit</td>
+                </tr>
+            </tbody>
+        </table>
     </div>
     </>
   )
